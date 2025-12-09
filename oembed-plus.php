e6f1f242d74f519fb12e902c7bfbbe210cb88ac8
Dennis Enderink <dennis@1815.nl>
2025-12-09 12:29:07 +0100

    style: apply formatting to oembed-plus.php file

--- oembed-plus.php
+++ oembed-plus.php
@@ -14,35 +14,44 @@
 use AchttienVijftien\Plugin\OembedPlus\Embed;
 use AchttienVijftien\Plugin\OembedPlus\Settings;
 
-add_filter('oembed_providers', static function (array $providers): array {
-	require_once __DIR__ . '/src/Embed.php';
-	return Embed::registerProviders($providers);
-});
-
-add_filter('oembed_fetch_url', static function ($provider_url): string {
-	if (strpos($provider_url, 'https://graph.facebook.com/v8.0/') !== 0) {
-		return $provider_url;
+add_filter(
+	'oembed_providers',
+	static function ( array $providers ): array {
+		require_once __DIR__ . '/src/Embed.php';
+		return Embed::registerProviders( $providers );
 	}
+);
 
-	require_once __DIR__ . '/src/Embed.php';
+add_filter(
+	'oembed_fetch_url',
+	static function ( $provider_url ): string {
+		if ( strpos( $provider_url, 'https://graph.facebook.com/v8.0/' ) !== 0 ) {
+			return $provider_url;
+		}
 
-	if (defined('OEMBED_PLUS_FACEBOOK_APP_ID') && defined('OEMBED_PLUS_FACEBOOK_SECRET')) {
-		$embed = new Embed(OEMBED_PLUS_FACEBOOK_APP_ID, OEMBED_PLUS_FACEBOOK_SECRET);
-	} elseif (
-		($app_id = get_option('oembed_facebook_app_id', null))
-		&& ($app_secret = get_option('oembed_facebook_app_secret', null))) {
-		$embed = new Embed($app_id, $app_secret);
-	} else {
-		return $provider_url;
+		require_once __DIR__ . '/src/Embed.php';
+
+		if ( defined( 'OEMBED_PLUS_FACEBOOK_APP_ID' ) && defined( 'OEMBED_PLUS_FACEBOOK_SECRET' ) ) {
+			$embed = new Embed( OEMBED_PLUS_FACEBOOK_APP_ID, OEMBED_PLUS_FACEBOOK_SECRET );
+		} elseif (
+			( $app_id = get_option( 'oembed_facebook_app_id', null ) ) && ( $app_secret = get_option( 'oembed_facebook_app_secret', null ) )
+		) {
+			$embed = new Embed( $app_id, $app_secret );
+		} else {
+			return $provider_url;
+		}
+
+		return $embed->processProviderUrls( $provider_url );
 	}
+);
 
-	return $embed->processProviderUrls($provider_url);
-});
-
-add_action('admin_init', static function (): void {
-	if (defined('OEMBED_PLUS_HIDE_ADMIN_UI') && !empty(OEMBED_PLUS_HIDE_ADMIN_UI)) {
-        return;
+add_action(
+	'admin_init',
+	static function (): void {
+		if ( defined( 'OEMBED_PLUS_HIDE_ADMIN_UI' ) && ! empty( OEMBED_PLUS_HIDE_ADMIN_UI ) ) {
+			return;
+		}
+		require_once __DIR__ . '/src/Settings.php';
+		Settings::runHook();
 	}
-	require_once __DIR__ . '/src/Settings.php';
-	Settings::runHook();
-});
+);
